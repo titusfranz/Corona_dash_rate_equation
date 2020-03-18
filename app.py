390dbfb27c04740da202bd9f6cd4cf5c0c943c2a
titusfranz <titus.franz@web.de>
2020-03-18 16:48:01 +0100

    add server

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div(
     children=[
         html.H1(children='Corona - Ratenmodell'),
@@ -184,4 +186,5 @@ def update_figure(infection_rate, recovery_rate, death_rate, symptomatic, asympt
     }
 
 
-app.run_server(debug=False)
+if __name__ == '__main__':
+    app.run_server(debug=True)
